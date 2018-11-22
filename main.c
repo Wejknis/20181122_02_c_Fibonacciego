fc5f19528cb250b4d1ee23d20a704dc003d78553
Wejknis <weja077@gmail.com>
2018-11-22 17:27:39 +0100

    sKOŃczone

--- main.c
+++ main.c
@@ -1,6 +1,56 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+void info() {
+    printf("Prgram wyświetlający liczby Ciągu Fibonnacciego\n");
+}
+
+int main(void) {
+    int n, i, czy_dalej = 1;
+
+    info();
+
+    /*
+    printf("podaj liczbę poczatkowa (naturalna): \n");
+    scanf("%i", &poczatkowa);
+    */
+
+    while (czy_dalej == 1) {
+
+        printf("ile cyfr zwrócić: \n");
+        scanf("%i", &n);
+
+        long long int ciag[n - 1];
+
+
+        //Dodanie 2 poczatkowych cyfr do ciagu
+        ciag[0] = 0;
+        ciag[1] = 1;
+
+        //Dodawanie kolejnych liczb fibo do tablicy
+
+        for (i = 2; i < n; i++) {
+
+            ciag[i] = ciag[i - 2] + ciag[i - 1];
+
+        }
+
+        //Wyswietlenie ciagu
+        printf("Liczby Fibonacciego: \n");
+
+        for (i = 0; i < n; i++) {
+
+            if (ciag[i] < 0) {
+                printf("BLAD - przekroczono zakres zmiennej dla %i liczby ciagu", i + 1);
+                break;
+            }
+            printf("%lli, ", ciag[i]);
+
+        }
+
+        printf("\n");
+
+        printf("Czy chcesz dalej liczyc? (1 - tak / 0 - nie: ");
+        scanf("%i", &czy_dalej);
+    }
+
 }
